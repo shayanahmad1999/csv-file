5532123bb61d56e06f74e0c6e352d895a13d1104
shayanahmad1999 <shayanahmad235@gmail.com>
2024-03-29 15:59:06 +0500

    the old query comment and the new query inert step by step if you want to insert data in one table with foreign key ow current table

--- functions.php
+++ functions.php
@@ -7,13 +7,59 @@ $conn = getdb();
 if (isset($_POST["Import"])) {
     $filename = $_FILES["file"]["tmp_name"];
 
+    // if ($_FILES["file"]["size"] > 0) {
+    //     $file = fopen($filename, "r");
+    //     while (($getData = fgetcsv($file, 10000, ",")) !== FALSE) {
+    //         $data = ucfirst(strtolower($getData[0]));
+    //         $sql = "INSERT INTO locations (lo_level, lo_county) VALUES ('1', '" . $data . "')";
+    //         //    values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."')";
+    //         $result = mysqli_query($conn, $sql);
+    //         if (!$result) {
+    //             echo "<script type=\"text/javascript\">
+    //                 alert(\"Invalid File: Please Upload CSV File.\");
+    //                 window.location = \"index.php\";
+    //             </script>";
+    //         } else {
+    //             echo "<script type=\"text/javascript\">
+    //                 alert(\"CSV File has been successfully Imported.\");
+    //                 window.location = \"index.php\";
+    //             </script>";
+    //         }
+    //     }
+    //     fclose($file);
+    // }
+
+
     if ($_FILES["file"]["size"] > 0) {
         $file = fopen($filename, "r");
         while (($getData = fgetcsv($file, 10000, ",")) !== FALSE) {
-            $data = ucfirst(strtolower($getData[0]));
-            $sql = "INSERT INTO locations (lo_level, lo_county) VALUES ('1', '" . $data . "')";
-            //    values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."')";
-            $result = mysqli_query($conn, $sql);
+            $county = ucfirst(strtolower($getData[0]));
+            $town = ucfirst(strtolower($getData[1]));
+            $postcode = ucfirst(strtolower($getData[2]));
+
+            $sql = "INSERT INTO locations (lo_level, lo_county) VALUES ('1', ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $county);
+            $result = $stmt->execute();
+            
+            if ($result) {
+                $county_id = $conn->insert_id;
+ 
+                $sql = "INSERT INTO locations (lo_level, lo_county, lo_town) VALUES ('2', ?, ?)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("is", $county_id, $town);
+                $result = $stmt->execute();
+                
+                if ($result) {
+                    $town_id = $conn->insert_id;
+
+                    $sql = "INSERT INTO locations (lo_level, lo_county, lo_town, lo_postcode) VALUES ('3', ?, ?, ?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("iis", $county_id, $town_id, $postcode);
+                    $result = $stmt->execute();
+                }
+            }
+    
             if (!$result) {
                 echo "<script type=\"text/javascript\">
                     alert(\"Invalid File: Please Upload CSV File.\");
@@ -28,6 +74,10 @@ if (isset($_POST["Import"])) {
         }
         fclose($file);
     }
+    
+    
+
+
 }
 
 if (isset($_POST["Export"])) {
